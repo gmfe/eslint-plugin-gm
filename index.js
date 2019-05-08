35bd9024d88bc1cce6dfc79829e13654cb5faf85
liyatang <liyatang@qq.com>
2019-05-08 19:33:39 +0800

    react version

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const createIndex = require('create-eslint-index');
-const importModules = require('import-modules');
-const rules = importModules('lib/rules', { camelize: false });
+const createIndex = require('create-eslint-index')
+const importModules = require('import-modules')
+const rules = importModules('lib/rules', { camelize: false })
 
 const recommendedRules = createIndex.createConfig({
   plugin: 'gmfe',
   field: 'meta.docs.recommended'
-}, rules);
+}, rules)
 
 module.exports = {
   rules,
@@ -18,11 +18,16 @@ module.exports = {
         'plugin:react/recommended'
       ],
       rules: {
-        'react/jsx-tag-spacing': ['error', {'beforeSelfClosing': 'never'}],
+        'react/jsx-tag-spacing': ['error', { 'beforeSelfClosing': 'never' }],
         'camelcase': 0,
         ...recommendedRules
       },
-      plugins: ['gmfe']
+      plugins: ['gmfe'],
+      settings: {
+        react: {
+          version: 'detect'
+        }
+      }
     }
   }
-};
+}
